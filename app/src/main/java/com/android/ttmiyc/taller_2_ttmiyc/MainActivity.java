512610b70171f84fb07226b0238f93a2094af3c7
celso <lenovog400s@localhost.localdomain>
2018-11-13 22:02:34 -0300

    Initial commit

---
+++ app/src/main/java/com/android/ttmiyc/taller_2_ttmiyc/MainActivity.java
@@ -0,0 +1,26 @@
+package com.android.ttmiyc.taller_2_ttmiyc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.id_btn){
+            
+        }
+    }
+}
